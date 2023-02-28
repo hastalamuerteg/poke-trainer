0a169fe7dca51b6309d56540959aa6ce75ba9efa
Gabriel Vicente <gabriel.vicent@hotmail.com>
2023-02-28 19:13:25 -0300

    added README

---
+++ README.md
@@ -0,0 +1,61 @@
+# Poke Trainer
+
+## Project 🚀🚀
+
+On Poke Trainer you can add Pokemons to your Pokedex and switch them by getting a new Pokemon from the list, you can also search for a Pokemon and check its details
+
+## Technologies 🧰🧰
+
+This project was created with
+
+- ReactJs
+- Typescript
+- Styled Componentss
+- Axios
+- Jotai
+- Jest
+- React Testing Library
+- Vite
+- ESLint
+- Yarn
+- Other 3rd Libraries
+
+## Concepts 💭💭
+
+This project makes use of concepts such as:
+
+- Functional Components
+- Hooks
+- State Machine
+- Global State Management
+- Public API
+- Others
+
+## Host ☁️☁️
+
+This project is being hosted by Vercel
+https://vercel.com/
+
+Check the live project with the link below
+
+[Check live project ->](https://poke-trainer.vercel.app/)
+
+## How To run the project 💻💻
+
+Clone the repository
+- Git clone https://github.com/hastalamuerteg/poke-trainer.git
+Install the dependencies
+- yarn
+env file
+- Create a .env.local file using the exact value from the .env.example file (VITE_BACKEND_URL='https://pokeapi.co/api/v2')
+Running the project locally
+- yarn dev
+Running tests
+- yarn test
+
+
+Thank you!
+
+###### This project may still suffer changes.
+
+###### Developed by Gabriel Vicente
--- src/components/DeveloperContacts/index.tsx
+++ src/components/DeveloperContacts/index.tsx
@@ -5,17 +5,32 @@ export const DeveloperContacts = () => {
   return (
     <S.Container>
       <li>
-        <S.Link href="https://www.linkedin.com/in/gabriel--vicente/" aria-label='Developer Linkedin' title="Developer Linkedin" id="Developer Linkedin">
+        <S.Link
+          href="https://www.linkedin.com/in/gabriel--vicente/"
+          aria-label="Developer Linkedin"
+          title="Developer Linkedin"
+          id="Developer Linkedin"
+        >
           <LinkedinLogo id="Developer Linkedin" />
         </S.Link>
       </li>
       <li>
-        <S.Link href="https://github.com/hastalamuerteg" aria-label='Developer Github' title="Developer Github" id="Developer Github">
+        <S.Link
+          href="https://github.com/hastalamuerteg"
+          aria-label="Developer Github"
+          title="Developer Github"
+          id="Developer Github"
+        >
           <GithubLogo id="Developer Github" />
         </S.Link>
       </li>
       <li>
-        <S.Link href="https://gabriel-vicente-dev.vercel.app/" aria-label='Developer Webiste' title="Developer Webiste" id="Developer Webiste">
+        <S.Link
+          href="https://gabriel-vicente-dev.vercel.app/"
+          aria-label="Developer Webiste"
+          title="Developer Webiste"
+          id="Developer Webiste"
+        >
           <GlobeIcon id="Developer Webiste" />
         </S.Link>
       </li>
